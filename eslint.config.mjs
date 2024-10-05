67d6053114e33d452078dc10909f090ecb6beccc
Nate Marshall <46655553+nate-marshall@users.noreply.github.com>
2024-10-05 15:50:31 -0500

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    files: ["**/*.js"], // Add this line to ensure it looks for .js files
+  },
   pluginJs.configs.recommended,
 ];
